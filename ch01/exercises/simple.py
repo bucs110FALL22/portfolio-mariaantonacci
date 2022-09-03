a7bc1702a8badf0ba45a67b92e1bb20c33acb026
Maria Antonacci <112440076+mariaantonacci@users.noreply.github.com>
2022-09-03 02:25:54 +0000

    ciao

---
+++ ch01/exercises/simple.py
@@ -0,0 +1,21 @@
+##part 1
+
+print("this is some python code")
+
+print(10*5)
+print(10**2)
+print(15/10)
+print(15//10)
+print(-15//10)
+print(15%10)
+print(15%15)
+print(10%10)
+print(0%0)
+print(10/15)
+#the result of line 12 is incorrect
+
+##part 2
+rate=1
+print("What is the current exchange rate for the Euro to Dollar?")
+print(rate)
+
