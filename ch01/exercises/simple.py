dcb124faf851fd02e1022734a842dcbd346111fa
Maria Antonacci <112440076+mariaantonacci@users.noreply.github.com>
2022-09-06 21:57:38 +0000

    exchange rate

--- ch01/exercises/simple.py
+++ ch01/exercises/simple.py
@@ -1,7 +1,5 @@
 ##part 1
 
-print("this is some python code")
-
 print(10*5)
 print(10**2)
 print(15/10)
@@ -10,12 +8,16 @@ print(-15//10)
 print(15%10)
 print(15%15)
 print(10%10)
-print(0%0)
+print(0%10)
 print(10/15)
-#the result of line 12 is incorrect
+
+#the result of line 12 is mathematically incorrect
 
 ##part 2
-rate=1
-print("What is the current exchange rate for the Euro to Dollar?")
-print(rate)
-
+rate= input("Please put the current exchange rate from euro to dollar: ")
+rate=int(rate)
+amount= input("Please put the amount of money you would like to exchange: ")
+amount=int(amount)
+total= (rate*amount)
+result= (total-3)
+print(result)
