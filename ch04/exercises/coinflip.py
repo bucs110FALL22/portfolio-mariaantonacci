d8367faac7673ae4fbfddd36d05ee627a52bde7f
Maria Antonacci <112440076+mariaantonacci@users.noreply.github.com>
2022-10-04 03:22:14 +0000

    ch04 exercise 2

---
+++ ch04/exercises/coinflip.py
@@ -0,0 +1,28 @@
+import turtle
+import random
+
+leo = turtle.Turtle()
+window = turtle.Screen()  
+window.bgcolor("purple")
+
+leo.shape("turtle")
+leo.color("green")
+
+while True:
+  coin = random.randint(1,2)
+  a=150
+  b=-150
+  if leo.xcor()>a or leo.xcor()<b or leo.ycor()>a or leo.ycor()<b:
+    break
+  elif coin==1:
+    print("Heads")
+    leo.left(90)
+  elif coin==2:
+    print("Tails")
+    leo.right(90)
+leo.forward(50)
+
+leo.pu()
+leo.goto(0,0)
+
+turtle.exitonclick()
