1679a7149241323afc5603e590106db4197206a3
Maria Antonacci <112440076+mariaantonacci@users.noreply.github.com>
2022-10-24 23:54:17 +0000

    gui exercise

---
+++ ch06/exercises/gui_classes.py
@@ -0,0 +1,17 @@
+class Tube:
+  def __init__(self):
+      self.color= "green"
+      self.height= 20
+      self.mario_in_tube=True
+
+class Mario:
+  def __init__(self):
+      self.Mario_num= 1 
+      self.lives= 3
+      self.move= True
+
+class Mushroom:
+  def __init__(self):
+      self.moving_forward=True
+      self.num_of_lives=1
+      self.is_alive=True
