3a1f5f39c2d57254e9401aff6e73133bbb070e7a
Maria Antonacci <112440076+mariaantonacci@users.noreply.github.com>
2022-10-31 22:44:43 +0000

    animal shelter

---
+++ ch07/exercises/shelter.py
@@ -0,0 +1,29 @@
+import time
+
+class Animal:
+  def __init__ (self, name, animal):
+    self.name= name
+    self.animal= animal
+    self.date= date
+
+  def getName(self):
+    return self.name
+
+  def getAnimal(self):
+    return self.animal
+
+  def getId(self):
+    return id(self)
+
+  def setAdopted(self):
+    return true
+
+  def getDate(self):
+    return date(self)
+
+  def main():
+     Bruno = Animal("Bruno", "dog", 1234)
+     Bruno.setAdopted()
+     print(id(scout))
+
+  main()
